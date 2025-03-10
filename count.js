fed273b22bd4b82b6395f87849afe52bcdeed8ad
Aniketkole27 <aniketkole07@gmail.com>
2025-03-10 19:37:57 +0530

    Add count.js for counting functionality with keyboard and click events

---
+++ count.js
@@ -0,0 +1,30 @@
+      const countNum = document.querySelector(".count");
+      const reset = document.querySelector("#btn");
+
+
+      
+      let num = Number(localStorage.getItem("greeting"));
+      countNum.innerHTML = num;
+
+        const counting = () =>{ 
+          num++;
+          countNum.innerHTML = num;
+          localStorage.setItem('greeting',num)
+        }
+
+      document.body.addEventListener('keydown',(e)=>{
+        if(e.code == 'Space'){
+          counting();
+        }
+      })
+
+      document.body.addEventListener('click',()=>{
+        counting();
+      });
+
+      reset.addEventListener('click',()=>{
+        num = -1;
+      })
+    
+    
+  
