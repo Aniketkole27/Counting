065a00c6afe5e89d75b9143202ea333e82172a56
Aniketkole27 <aniketkole07@gmail.com>
2025-03-10 19:55:06 +0530

    Refactor counting function to improve readability and reset logic

--- count.js
+++ count.js
@@ -1,29 +1,27 @@
-      const countNum = document.querySelector(".count");
-      const reset = document.querySelector("#btn");
+const countNum = document.querySelector(".count");
+const reset = document.querySelector("#btn");
 
+let num = Number(localStorage.getItem("greeting")) || 0;
+countNum.innerHTML = num;
+const counting = () => {
+  num++;
+  countNum.innerHTML = num;
+  localStorage.setItem("greeting", num);
+};
 
-      
-      let num = Number(localStorage.getItem("greeting"));
-      countNum.innerHTML = num;
-        const counting = () =>{ 
-          num++;
-          countNum.innerHTML = num;
-          localStorage.setItem('greeting',num)
-        }
+document.body.addEventListener("keydown", (e) => {
+  if (e.code == "Space" && document.activeElement !== reset) {
+    counting();
+  }
+});
 
-      document.body.addEventListener('keydown',(e)=>{
-        if(e.code == 'Space'){
-          counting();
-        }
-      })
+document.body.addEventListener("click", () => {
+  counting();
+});
 
-      document.body.addEventListener('click',()=>{
-        counting();
-      });
-
-      reset.addEventListener('click',()=>{
-        num = -1;
-      })
-    
-    
-  
+reset.addEventListener("click", () => {
+  // num = -1; // Reset to 0
+  num = 0; // Reset num to 0
+  countNum.innerHTML = num; // Update the display
+  localStorage.setItem("greeting", num);
+});
