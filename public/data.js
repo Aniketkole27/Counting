833d4d6772a9fa5fe36bc75c8fb234c8c61807d0
Aniketkole27 <aniketkole07@gmail.com>
2025-03-31 03:14:01 +0530

    Refactor data.js to improve final count display and add star rating feature

--- public/data.js
+++ public/data.js
@@ -1,24 +1,38 @@
-import { getFinalCount } from "./count.js";
+import { getFinalCount } from "../count.js";
 
 console.log(getFinalCount());
-
 let finalCount = getFinalCount();
 
 const allBoxes = document.querySelectorAll(".box");
-const arr  = [];
-allBoxes.forEach((e)=>{
-    arr.push(e);
-})
+const arr = [];
+allBoxes.forEach((e) => {
+  arr.push(e);
+});
 
 // console.log(arr)
 const day = new Date();
-console.log(day.getDate());
 
-const updataBasedOnData = ()=>{
-    const h2 = document.createElement("h2");
-    h2.innerHTML = finalCount
-    let add = arr[day.getDate()-1].appendChild(h2)
-    // add.appendChild(h2)
-}
+const updataBasedOnData = () => {
+  const h2 = document.createElement("h2");
+  const div = document.createElement("div");
+  div.setAttribute("class", "rewords");
+  h2.innerHTML = finalCount;
+
+  // putting it in perticular Day
+  const toDay =  arr[day.getDate() - 1];
+  toDay.appendChild(h2);
+  let starCount = Math.floor(finalCount / 1000);
+  console.log(starCount)
+     for(let i = 0; i<starCount;i++){
+        console.log("h")
+        if(i < 5){
+            const span = document.createElement("span");
+            span.innerHTML = "⭐";
+            div.appendChild(span);
+        }
+     }
+
+  toDay.appendChild(div);
+};
 
 updataBasedOnData();
