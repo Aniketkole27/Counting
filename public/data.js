e13624966c0b05bbd0adac77b24dd53aaacc2c21
Aniketkole27 <aniketkole07@gmail.com>
2025-03-31 23:59:59 +0530

    Refactor data.js to improve data handling and UI updates with localStorage integration

--- public/data.js
+++ public/data.js
@@ -1,38 +1,66 @@
 import { getFinalCount } from "../count.js";
-
-console.log(getFinalCount());
-let finalCount = getFinalCount();
-
 const allBoxes = document.querySelectorAll(".box");
+let finalCount = getFinalCount();
+const day = new Date();
+
 const arr = [];
 allBoxes.forEach((e) => {
   arr.push(e);
 });
 
-// console.log(arr)
-const day = new Date();
-
 const updataBasedOnData = () => {
-  const h2 = document.createElement("h2");
-  const div = document.createElement("div");
-  div.setAttribute("class", "rewords");
-  h2.innerHTML = finalCount;
-
-  // putting it in perticular Day
-  const toDay =  arr[day.getDate() - 1];
-  toDay.appendChild(h2);
-  let starCount = Math.floor(finalCount / 1000);
-  console.log(starCount)
-     for(let i = 0; i<starCount;i++){
-        console.log("h")
-        if(i < 5){
-            const span = document.createElement("span");
-            span.innerHTML = "⭐";
-            div.appendChild(span);
-        }
-     }
-
-  toDay.appendChild(div);
+  initializeArray();
+  updateValue(day.getDate() - 1, finalCount);
 };
 
 updataBasedOnData();
+
+function initializeArray() {
+  if (!localStorage.getItem("dataArray")) {
+    let numbers = new Array(31).fill(0); 
+    // console.log(numbers)
+    localStorage.setItem("dataArray", JSON.stringify(numbers));
+  }
+}
+
+function updateValue(index, update) {
+  let numbers = JSON.parse(localStorage.getItem("dataArray"));
+  numbers[index] = update;
+  localStorage.setItem("dataArray", JSON.stringify(numbers)); 
+  updateUi(numbers);
+}
+// resetArray()
+
+function resetArray() {
+  localStorage.removeItem("dataArray");
+  initializeArray();
+}
+
+function updateUi(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "rewords");
+
+    if (numbers[i] !== 0) {
+      let h2 = document.createElement("h2");
+      h2.innerHTML = numbers[i];
+      arr[i].appendChild(h2);
+      arr[i].appendChild(div)
+      addStar(div,numbers[i]);
+    }
+    // console.log(numbers[i])
+  }
+}
+
+function addStar(div,c){
+  console.log(div)
+  let starCount = Math.floor(c / 1000);
+  for (let i = 0; i < starCount; i++) {
+    // console.log("h")
+    if (i < 5) {
+      const span = document.createElement("span");
+      span.innerHTML = "⭐";
+      div.appendChild(span);
+    }
+  }
+}
