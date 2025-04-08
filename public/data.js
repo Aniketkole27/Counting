1f01d4d8e9e1b172420aa148dfdaebfcee4e7d2d
Aniketkole27 <aniketkole07@gmail.com>
2025-04-08 23:51:56 +0530

    Refactor data.js to streamline data handling and update UI with exported array

--- public/data.js
+++ public/data.js
@@ -1,47 +1,46 @@
 import { getFinalCount } from "../count.js";
 const allBoxes = document.querySelectorAll(".box");
-let finalCount = getFinalCount();
 const day = new Date();
 
 const arr = [];
 allBoxes.forEach((e) => {
   arr.push(e);
 });
+let exportedArray = getFinalCount()
+updateUi(exportedArray)
 
-const updataBasedOnData = () => {
-  initializeArray();
-  updateValue(day.getDate() - 1, finalCount);
-};
+// const updataBasedOnData = () => {
+//   initializeArray();
+//   updateValue(day.getDate() - 1, finalCount);
+// };
 
-updataBasedOnData();
+// updataBasedOnData();
 
-function initializeArray() {
-  if (!localStorage.getItem("dataArray")) {
-    let numbers = new Array(31).fill(0); 
-    // console.log(numbers)
-    localStorage.setItem("dataArray", JSON.stringify(numbers));
-  }
-}
+// function initializeArray() {
+//   if (!localStorage.getItem("dataArray")) {
+//     let numbers = new Array(31).fill(0); 
+//     localStorage.setItem("dataArray", JSON.stringify(numbers));
+//   }
+// }
 
-function updateValue(index, update) {
-  let numbers = JSON.parse(localStorage.getItem("dataArray"));
-  numbers[index] = update;
-  localStorage.setItem("dataArray", JSON.stringify(numbers)); 
-  updateUi(numbers);
-}
-// resetArray()
+// function updateValue(index, update) {
+//   let numbers = JSON.parse(localStorage.getItem("dataArray"));
+//   numbers[index] = update;
+//   localStorage.setItem("dataArray", JSON.stringify(numbers)); 
+//   updateUi(numbers);
+// }
 
-function resetArray() {
-  localStorage.removeItem("dataArray");
-  initializeArray();
-}
+// function resetArray() {
+//   localStorage.removeItem("dataArray");
+//   initializeArray();
+// }
 
 function updateUi(numbers) {
   for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== 0) {
     const div = document.createElement("div");
     div.setAttribute("class", "rewords");
 
-    if (numbers[i] !== 0) {
       let h2 = document.createElement("h2");
       h2.innerHTML = numbers[i];
       arr[i].appendChild(h2);
